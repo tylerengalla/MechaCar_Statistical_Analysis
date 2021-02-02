5760c8b819d182c3e1adb83b5ee83dac7a091118
Tyler Engalla <tcengalla@gmail.com>
2021-02-02 16:55:14 -0600

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,6 +2,7 @@ library(jsonlite)
 library(tidyverse)
 library(dplyr)
 
+## Deliverable 1 -----
 # Import Data 
 MechaCar_mpg <- read_csv("challenge_data/MechaCar_mpg.csv")
 
@@ -10,3 +11,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Determing p-value and r-squared value with summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
+
+
+## Deliverable 2 -----
+#Import Suspension_Coil.csv
+Suspension_Coil <- read_csv("challenge_data/Suspension_Coil.csv")
+
+# Create a total_summary df using summarize() func of PSI column
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Create a lot_summary df using group_by() 
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
